9df06323ec97a5eb432b633c6e67c7f76bb8504d
vhamelbravo <vicentehamelbravo@gmail.com>
2023-09-05 22:55:13 -0300

    Update

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,16 @@
 import backgroundImage from './assets/logo.jpg'
 
 function Hero() {
+	const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+
+    if (element) {
+      window.scrollTo({
+        behavior: 'smooth',
+        top: element.offsetTop,
+      });
+    }
+  };
 	return (
 <div id="inicio">
 		<div className="hero min-h-screen" style={{backgroundImage: `url(${backgroundImage})`,marginTop:  0 }}>
@@ -9,7 +19,7 @@ function Hero() {
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold">Elige accesorios calidez y calidad para completar tu outfit del día a día</h1>
       <p className="mb-5">MICARAVANCERAI es un tienda con una pequeña trayectoria comercial pero con una amplia experiencia, conocimiento y estilo de cada producto seleccionado para su venta. </p>
-      <a href="#tienda" className="btn btn-primary">Ver Productos</a>
+      <a  onClick={() => scrollToSection('tienda')} style={{ cursor: 'pointer' }}className="btn btn-primary">Ver Productos</a>
     </div>
   </div>
 </div>
