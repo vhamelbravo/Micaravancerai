0cfe1d3cef59159afffa3b412d4c8f6e406b68e6
vhamelbravo <vicentehamelbravo@gmail.com>
2023-10-05 22:46:40 -0300

    Store backend test

--- src/Header.js
+++ src/Header.js
@@ -136,7 +136,7 @@ function Header({ selectedProducts, setSelectedProducts }) {
 		      data-item-price={selectedProducts.map((product) => product.fields.precio.replace(/\./g, "").replace(",", "."))}
         data-item-image={selectedProducts.map((product) => `https:${product.fields.imagenes.fields.file.url}`).join(',')}
         data-item-name={selectedProducts.map((product) => product.fields.nombre).join(',')}
-        data-item-url="https://micaravancerai.cl"
+        data-item-url={`https://micaravancerai.cl/${product.fields.slug}`}}
 		      >Comprar </button>
                     </div>
                   </div>
