e73336dee671df58797d23edacc2d011a817f7ba
vhamelbravo <vicentehamelbravo@gmail.com>
2023-10-05 23:47:54 -0300

    Size

--- src/Store.js
+++ src/Store.js
@@ -73,7 +73,7 @@ function Store({ selectedProducts, setSelectedProducts }) {
                         <h2 className="card-title">
                           {productData.fields.nombre}
                         </h2>
-                        <p className="max-w-1/2">
+                        <p className="w-3/4">
                           {documentToReactComponents(
                             productData.fields.descripcion,
                           )}
